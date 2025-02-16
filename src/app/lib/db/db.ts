7483ced2ea854e7d2f9b1c41602429322a29f84d
Atul /maurya <103453238+atul-5242@users.noreply.github.com>
2025-02-16 23:11:35 +0530

    Update db.ts

--- src/app/lib/db/db.ts
+++ src/app/lib/db/db.ts
@@ -1,8 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
 import postgres from 'postgres';
 
-const queryString = "postgresql://neondb_owner:npg_LXhe0viR1GoP@ep-plain-snowflake-a8eiomaj-pooler.eastus2.azure.neon.tech/neondb?sslmode=require";
-// const queryString = process.env.DATABASE_URL as string;
+
+const queryString = process.env.DATABASE_URL as string;
 export const connection = postgres(queryString);
 
 export const db = drizzle(connection);
